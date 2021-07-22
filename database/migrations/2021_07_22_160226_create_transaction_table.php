23e92382e95e0e6b61f2f1daa87def5423fc22d4
KryPtoN <sed.mamad.khalili@gmail.com>
2021-07-22 20:36:18 +0430

    create transaction migration table

---
+++ database/migrations/2021_07_22_160226_create_transaction_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction', function (Blueprint $table) {
+            $table->id()->autoIncrement()->unique()->primary();
+            $table->integer('price');
+            $table->string('tracking_code');
+            $table->integer('response_code');
+            $table->enum('status' , ['payment', 'paid','failed']);
+            $table->integer('request_code');
+            $table->timestamp('paid_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction');
+    }
+}
