e68a7d381e9d8cd902aa5a643a58fa2104eb0933
KryPtoN <sed.mamad.khalili@gmail.com>
2021-07-22 19:31:52 +0430

    create admin migration table

---
+++ database/migrations/2021_07_22_143717_admin.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Admin extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('name', 30);
+            $table->string('family', 35);
+            $table->string('phone', 11);
+            $table->string('email', 50);
+            $table->string('password', 9);
+            $table->string('picture', 100)->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin');
+    }
+}
