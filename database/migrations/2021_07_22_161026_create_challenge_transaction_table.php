47acbd241a097ada5822f5c8daf17f7a9c193c9d
KryPtoN <sed.mamad.khalili@gmail.com>
2021-07-22 20:42:05 +0430

    create challenge_transaction table

---
+++ database/migrations/2021_07_22_161026_create_challenge_transaction_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChallengeTransactionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('challenge_transaction', function (Blueprint $table) {
+            $table->id()->autoIncrement()->unique()->primary();
+            $table->foreignId('challenge_id');
+            $table->foreignId('transaction_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('challenge_transaction');
+    }
+}
