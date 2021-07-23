5eea8aa97b5c6fe44c276d80b65233b73db1fe3f
Mohammad Foroughi <foroughi1380@gmail.com>
2021-07-23 22:47:29 +0430

    add main theme scripts

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,12 +1,59 @@
 <!DOCTYPE html>
-<html>
+<html class="loading" lang="en" data-textdirection="rtl">
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+    <meta charset="utf-8"/>
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet"/>
     <script src="{{ mix('/js/app.js') }}" defer></script>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <link rel="apple-touch-icon" href="/theme/web/app-assets/images/ico/apple-icon-120.png">
+    <link rel="shortcut icon" type="image/x-icon" href="/theme/web/app-assets/images/ico/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600" rel="stylesheet">
+
+    <!-- BEGIN: Vendor CSS-->
+    <link rel="stylesheet" type="text/css" href="/theme/web/app-assets/vendors/css/vendors-rtl.min.css">
+    <!-- END: Vendor CSS-->
+
+    <!-- BEGIN: Theme CSS-->
+    <link rel="stylesheet" type="text/css" href="/theme/web/app-assets/css-rtl/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="/theme/web/app-assets/css-rtl/bootstrap-extended.css">
+    <link rel="stylesheet" type="text/css" href="/theme/web/app-assets/css-rtl/colors.css">
+    <link rel="stylesheet" type="text/css" href="/theme/web/app-assets/css-rtl/components.css">
+    <link rel="stylesheet" type="text/css" href="/theme/web/app-assets/css-rtl/themes/dark-layout.css">
+    <link rel="stylesheet" type="text/css" href="/theme/web/app-assets/css-rtl/themes/semi-dark-layout.css">
+
+    <!-- BEGIN: Page CSS-->
+    <link rel="stylesheet" type="text/css" href="/theme/web/app-assets/css-rtl/core/menu/menu-types/vertical-menu.css">
+    <link rel="stylesheet" type="text/css" href="/theme/web/app-assets/css-rtl/core/colors/palette-gradient.css">
+    <link rel="stylesheet" type="text/css" href="/theme/web/app-assets/css-rtl/pages/invoice.css">
+    <!-- END: Page CSS-->
+
+    <!-- BEGIN: Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="/theme/web/app-assets/css-rtl/custom-rtl.css">
+    <link rel="stylesheet" type="text/css" href="/theme/web/assets/css/style-rtl.css">
+    <!-- END: Custom CSS-->
 </head>
 <body>
 @inertia
+
+
+<!-- BEGIN: Vendor JS-->
+<script src="/theme/web/app-assets/vendors/js/vendors.min.js"></script>
+<!-- BEGIN Vendor JS-->
+
+<!-- BEGIN: Page Vendor JS-->
+<!-- END: Page Vendor JS-->
+
+<!-- BEGIN: Theme JS-->
+<script src="/theme/web/app-assets/js/core/app-menu.js"></script>
+<script src="/theme/web/app-assets/js/core/app.js"></script>
+<script src="/theme/web/app-assets/js/scripts/components.js"></script>
+<!-- END: Theme JS-->
+
+<!-- BEGIN: Page JS-->
+<script src="/theme/web/app-assets/js/scripts/pages/invoice.js"></script>
+<!-- END: Page JS-->
 </body>
 </html>
