8fc5c2435a0fe60b5aabd6874b2d731b5b3ed7a5
Mohammad Foroughi <foroughi1380@gmail.com>
2021-07-24 17:13:41 +0430

    fix foother close san tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -44,7 +44,7 @@
 <!-- BEGIN: Footer-->
 <footer class="footer footer-static footer-light">
     <p class="clearfix blue-grey lighten-2 mb-0"><span class="float-md-left d-block d-md-inline-block mt-25">
-            تمامی حقوق این سایت متعلق به ممدا است
+            تمامی حقوق این سایت متعلق به ممدا است </span>
     </p>
 </footer>
 
