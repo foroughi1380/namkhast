329493873125f1ea835283e6ec4ba04b25864f17
Mohammad Foroughi <foroughi1380@gmail.com>
2021-07-24 23:21:21 +0430

    fix the v-model not world in vue 3

--- resources/js/Shared/Components/IconInputText.vue
+++ resources/js/Shared/Components/IconInputText.vue
@@ -1,6 +1,6 @@
 <template>
   <fieldset class="form-label-group form-group position-relative has-icon-left">
-    <input type="text" class="form-control" :id="name" :name="name" :placeholder="placeholder" :value="value" @input="$emit('input', $event.target.value)">
+    <input :type="type ? type : 'text'" class="form-control" :id="name" :name="name" :placeholder="placeholder" :value="modelValue" @input="inputChange">
     <div class="form-control-position">
       <i :class="['feather' , icon]"></i>
     </div>
@@ -11,7 +11,12 @@
 <script>
 export default {
   name: "IconInputText",
-  props : ['name' , 'icon' , 'placeholder' , 'label' , 'value']
+  props : ['name' , 'icon' , 'placeholder' , 'label' , 'modelValue' , 'type'],
+  methods:{
+    inputChange(e){
+      this.$emit('update:modelValue', e.target.value);
+    }
+  }
 }
 </script>
 
