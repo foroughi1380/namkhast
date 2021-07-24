cc5216c0c93bb5891ed4380248a7f05a73ca2007
Mohammad Foroughi <foroughi1380@gmail.com>
2021-07-24 17:15:30 +0430

    create index of login with userRegisterLogin tempalte

--- app/Http/Controllers/web/ReLoController.php
+++ app/Http/Controllers/web/ReLoController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\web;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ReLoController extends Controller
 {
-    //
+    function loginIndex(){
+        Inertia::setRootView("userRegisterLogin");
+        return Inertia::render("Web/login");
+    }
 }
