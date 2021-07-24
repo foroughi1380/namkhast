285ce1a069214a1d7560ddd6de2072494555ed81
Mohammad Foroughi <foroughi1380@gmail.com>
2021-07-24 17:35:10 +0430

    create IconInputText componet for use to register and login page

---
+++ resources/js/Shared/Components/IconInputText.vue
@@ -0,0 +1,20 @@
+<template>
+  <fieldset class="form-label-group form-group position-relative has-icon-left">
+    <input type="text" class="form-control" :id="name" :name="name" :placeholder="placeholder" :value="value" @input="$emit('input', $event.target.value)">
+    <div class="form-control-position">
+      <i :class="['feather' , icon]"></i>
+    </div>
+    <label :for="name" v-text="label"></label>
+  </fieldset>
+</template>
+
+<script>
+export default {
+  name: "IconInputText",
+  props : ['name' , 'icon' , 'placeholder' , 'label' , 'value']
+}
+</script>
+
+<style scoped>
+
+</style>
