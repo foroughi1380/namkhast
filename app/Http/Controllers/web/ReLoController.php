872fc8c87b41fc90e39c85c36b5a7ad3c9deaec0
Mohammad Foroughi <foroughi1380@gmail.com>
2021-07-24 23:44:46 +0430

    create user information and authantication Controller

--- app/Http/Controllers/web/ReLoController.php
+++ app/Http/Controllers/web/ReLoController.php
@@ -3,13 +3,24 @@
 namespace App\Http\Controllers\web;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\SendSms;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Inertia\Inertia;
+
 
 class ReLoController extends Controller
 {
     function loginIndex(){
+        //SendSms::sendConfirmCode("09162943094");
         Inertia::setRootView("userRegisterLogin");
         return Inertia::render("Web/login");
+    }
+
+    function login(LoginRequest $request){
+        return Inertia::render("Web/login" , [
+           '' => "all thing is ok"
+        ]);
     }
 }
