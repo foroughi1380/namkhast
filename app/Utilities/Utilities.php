059a0a3e8d91e361bfbac7d553cf0c5fc082919f
Mohammad Foroughi <foroughi1380@gmail.com>
2021-07-26 01:17:09 +0430

    wirte captcha check function

--- app/Utilities/Utilities.php
+++ app/Utilities/Utilities.php
@@ -4,7 +4,16 @@
 namespace App\Utilities;
 
 
+use ReCaptcha\ReCaptcha;
+
 class Utilities
 {
-
+    static function checkRecaptcha($token , $action , $ip){
+        $recaptcha = new ReCaptcha(env("MIX_RECAPTCHA3_SECRET"));
+        $recaptcha->setExpectedAction($action);
+        $recaptcha->setExpectedHostname(env("APP_HOSTNAME"));
+        $recaptcha->setScoreThreshold(0.6);
+        $res = $recaptcha->verify($token , $ip);
+        return $res->isSuccess();
+    }
 }
