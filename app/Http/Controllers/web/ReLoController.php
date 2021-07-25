ef2eb7d525a5af5a140512f985a879659d9826fd
Mohammad Foroughi <foroughi1380@gmail.com>
2021-07-26 01:16:16 +0430

    write login function (test mode)

--- app/Http/Controllers/web/ReLoController.php
+++ app/Http/Controllers/web/ReLoController.php
@@ -4,9 +4,14 @@ namespace App\Http\Controllers\web;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
-use App\Http\SendSms;
+use App\Models\User;
+use App\Utilities\SendSms;
+use App\Utilities\Utilities;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 
 
@@ -19,8 +24,34 @@ class ReLoController extends Controller
     }
 
     function login(LoginRequest $request){
-        return Inertia::render("Web/login" , [
-           '' => "all thing is ok"
-        ]);
+        // check request token validation
+        if (! Utilities::checkRecaptcha($request->get('token') , 'login' , $request->ip())){
+            return Inertia::render("Web/login" , [
+                'errors' => ['token'=>'کپچای شما رد شد بهتر است صفجه را مجددا بارگذاری کنید.']
+            ]);
+        }
+
+        /** @var User $user */
+        $user = User::query()->where('phone' , '=' , $request->get("phone"))->first();
+        if (!$request->has("code")){
+            //$user->cc = bcrypt(SendSms::sendConfirmCode($user->phone));
+            $user->cc = bcrypt(1234);
+            $user->save();
+
+            return Inertia::render("Web/login" , [
+               'conf' => true
+            ]);
+        }else{
+            if (Hash::check($request->get("code") , $user->cc)){
+                $user->cc = "";
+                $user->save();
+                Auth::login($user);
+                return Inertia::render("Web/login" , ['refresh'=>true]);
+            }
+
+            return Inertia::render("Web/login" , [
+                'errors' => ["code"=>"کد وارد شده صحیح نمیباشد"]
+            ]);
+        }
     }
 }
