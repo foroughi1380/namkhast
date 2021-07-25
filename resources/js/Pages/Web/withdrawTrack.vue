a7853892b24ea07e5adba89e3ce921ed72e6d8d0
KryPtoN <sed.mamad.khalili@gmail.com>
2021-07-25 16:56:14 +0430

    move dataTable scripts temprerory in app view

---
+++ resources/js/Pages/Web/withdrawTrack.vue
@@ -0,0 +1,18 @@
+<template>
+  <Head>
+    <title>پیگیری درخواست برداشت</title>
+  </Head>
+</template>
+
+<script>
+import appLayout from "../../Shared/appLayout";
+
+export default {
+  name: "withrawTrack",
+  layout: appLayout
+}
+</script>
+
+<style scoped>
+
+</style>
