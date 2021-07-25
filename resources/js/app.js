46aef4174450272486023ad61e38328746cc55c1
Mohammad Foroughi <foroughi1380@gmail.com>
2021-07-25 18:05:33 +0430

    add VueRecaptcha create token method

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,26 @@
 require('./bootstrap');
-import { createApp, h } from 'vue'
-import { createInertiaApp , Link } from '@inertiajs/inertia-vue3'
-import { Head } from '@inertiajs/inertia-vue3'
+import {createApp, h} from 'vue'
+import {createInertiaApp, Head, Link} from '@inertiajs/inertia-vue3'
+import {VueReCaptcha} from 'vue-recaptcha-v3'
 
 var vuevue;
 createInertiaApp({
-    title : title => `${title} | نام خواست `,
+    title: title => `${title} | نام خواست `,
     resolve: name => require(`./Pages/${name}`),
-    setup({ el, app, props, plugin }) {
-        vuevue = createApp({ render: () => h(app, props)});
+    setup({el, app, props, plugin}) {
+        vuevue = createApp({render: () => h(app, props)});
         vuevue.use(plugin)
-        vuevue.component("Link" , Link)
-        vuevue.component("Head" , Head)
+        vuevue.component("Link", Link)
+        vuevue.component("Head", Head)
+        vuevue.use(VueReCaptcha, {siteKey: process.env.MIX_RECAPTCHA3_SITE})
+        vuevue.mixin({
+            methods: {
+                async generateRecaptchaToken(action) {
+                    await this.$recaptchaLoaded()
+                    return await this.$recaptcha(action);
+                }
+            }
+        })
         vuevue.mount(el)
     },
 })
