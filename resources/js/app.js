828a57fcfc68aade192dd8eb24a87cb47ef7e5ed
Mohammad Foroughi <foroughi1380@gmail.com>
2021-07-26 04:35:21 +0430

    adding vue toast notification to vue and create tow method in mix in

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,14 +2,19 @@ require('./bootstrap');
 import {createApp, h} from 'vue'
 import {createInertiaApp, Head, Link} from '@inertiajs/inertia-vue3'
 import {VueReCaptcha} from 'vue-recaptcha-v3'
+import VueToast from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-default.css';
 
-var vuevue;
+
 createInertiaApp({
     title: title => `${title} | نام خواست `,
     resolve: name => require(`./Pages/${name}`),
     setup({el, app, props, plugin}) {
-        vuevue = createApp({render: () => h(app, props)});
+        let vuevue = createApp({render: () => h(app, props)});
         vuevue.use(plugin)
+        vuevue.use(VueToast , {
+            position : "top-left"
+        })
         vuevue.component("Link", Link)
         vuevue.component("Head", Head)
         vuevue.use(VueReCaptcha, {siteKey: process.env.MIX_RECAPTCHA3_SITE})
@@ -22,10 +27,24 @@ createInertiaApp({
                 checkPhoneIsValid(phone){
                     let regex = /^09\d{9}$/
                     return regex.test(phone)
+                },
+                toast(message , type){
+                    this.$toast.open({
+                        message: message,
+                        type: type,
+
+                    });
+                },
+                showToasts(messages){
+                    if (messages)
+                    for (let t of this.toasts){
+                        this.toast(t.message , t.type)
+                    }
                 }
             }
         })
         vuevue.mount(el)
+
     },
 })
 
