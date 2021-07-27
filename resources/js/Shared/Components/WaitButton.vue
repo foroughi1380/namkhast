106cc4af2ef2be25cde4344c5f46a3585e78a3e4
Mohammad Foroughi <foroughi1380@gmail.com>
2021-07-28 03:08:59 +0430

    create a button with wait animation custom component

---
+++ resources/js/Shared/Components/WaitButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="onClick" class="btn btn-primary  btn-inline">
+    <span  v-if="! wait" v-text="text"></span>
+    <hollow-dots-spinner v-else
+                         :animation-duration="1000"
+                         :dot-size="10"
+                         :dots-num="3"
+                         color="#ffffff"
+    />
+  </button>
+</template>
+
+<script>
+export default {
+  name: "WaitButton",
+  props : ['wait', 'text' , 'onClick']
+}
+</script>
+
+<style scoped>
+
+</style>
