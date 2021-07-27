b2676858f2380fdc701876731f149d6dda950b69
Mohammad Foroughi <foroughi1380@gmail.com>
2021-07-28 02:01:56 +0430

    adding custom input file

---
+++ resources/js/Shared/Components/SimpleInputFile.vue
@@ -0,0 +1,24 @@
+<template>
+  <fieldset class="form-group col-3 mt-2">
+    <div class="custom-file">
+      <input type="file" class="custom-file-input" :id="name" :accept="accept" @input="inputChange" :value="modelValue">
+      <label class="custom-file-label" :name="name" :for="name">{{label}}</label>
+    </div>
+  </fieldset>
+</template>
+
+<script>
+export default {
+  name: "SimpleInputFile",
+  props : ["name" , "label" , "accept" , "modelValue"],
+  methods:{
+    inputChange(e){
+      this.$emit('update:modelValue', e.target.files[0]);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Shared/Components/SimpleTextBox.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="form-group">
+    <div class="controls">
+      <label>{{ label }}</label>
+      <input :type="type ? type : 'text'" :name="name" class="form-control" :placeholder="placeholder" :value="modelValue" :maxlength="maxlen" @input="this.$emit('update:modelValue', e.target.value)">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SimpleTextBox",
+  props : ['name' , 'placeholder' , 'label' , 'modelValue' , 'type' , 'maxlen']
+}
+</script>
+
+<style scoped>
+
+</style>
