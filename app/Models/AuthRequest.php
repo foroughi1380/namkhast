23c5bcfd6755274cfdcc105df2bbd38a54b2f684
Mohammad Foroughi <foroughi1380@gmail.com>
2021-07-28 19:04:39 +0430

    create auth request model

---
+++ app/Models/AuthRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+
+class AuthRequest extends Model
+{
+    use HasFactory;
+    protected $table = "authentication_request";
+    protected $primaryKey = "user_id";
+
+    protected $fillable = [
+      "status",
+      "national_code",
+      "description"
+    ];
+
+    protected $hidden = [
+        "try",
+        "nc_picture",
+        "user_id"
+    ];
+
+    public function getStatusAttribute($value){
+        if ($value === 'block' && Carbon::createFromTimestamp($this->getUpdatedAtColumn())->addDays(3)->isBefore(now())){
+            return "unblock";
+        }
+        return $value;
+    }
+
+    public function getNationalCodePicture()
+    {
+        return Storage::get($this->picture);
+    }
+}
