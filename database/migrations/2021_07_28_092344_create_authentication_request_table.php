36695f2456740708489652d95937e70729f70892
Mohammad Foroughi <foroughi1380@gmail.com>
2021-07-28 17:02:09 +0430

    create auth request table

---
+++ database/migrations/2021_07_28_092344_create_authentication_request_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuthenticationRequestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authentication_request', function (Blueprint $table) {
+            $table->foreignId("user_id")->constrained("user")->cascadeOnDelete()->cascadeOnUpdate();
+            $table->unsignedSmallInteger("try")->default(1);
+            $table->enum("status" , ["accept" , "reject" , "pending" , "block"])->default("pending");
+            $table->string("national_code" , 10)->unique();
+            $table->string('nc_picture', 100);
+            $table->string("description")->default("");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authentication_request');
+    }
+}
