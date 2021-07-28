6046d577b09f42ec41b9c284d9896a41ba189090
KryPtoN <sed.mamad.khalili@gmail.com>
2021-07-28 10:03:42 +0430

    create and store method for Admin

--- app/Http/Controllers/AC/AdminController.php
+++ app/Http/Controllers/AC/AdminController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\AC;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AcAdminCreateRequest;
 use App\Http\Requests\AcAdminEditRequest;
 use App\Models\Admin;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Inertia\Inertia;
 
 class AdminController extends Controller
@@ -16,6 +19,21 @@ class AdminController extends Controller
         return Inertia::render('AC/admins', [
             'admins' => $admins
         ]);
+    }
+
+    public function create(){
+        return Inertia::render('AC/adminCreate');
+    }
+
+    public function store(AcAdminCreateRequest $request){
+
+        if(DB::table('admin')->insert(['name'=>$request->get('name'),'family'=>$request->get('family'),'phone'=>$request->get('phone'),'email'=>$request->get('email'),'password'=>Hash::make($request->get('password'))])){
+            return Inertia::render("AC/adminCreate");
+        }else{
+            return Inertia::render("AC/adminCreate" , [
+                'errors' => ['term' => 'متاسفانه خطایی در سامانه رخ داده است.']
+            ]);
+        }
     }
 
     public function edit($id)
@@ -32,7 +50,7 @@ class AdminController extends Controller
             "name" => "required|max:50",
             "family" => "required|max:70",
             "phone" => "required|digits:11|regex:/^09\d{9}$/|unique:admin,phone,". $id,
-            "email" => "required|unique:admin,email,". $id,
+            "email" => "required|email|unique:admin,email,". $id,
         ]);
 
         $admin = Admin::query()->find($id);
