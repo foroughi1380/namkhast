1ff5bd0809137e63280db9ebfab2ef9d0d564d14
KryPtoN <sed.mamad.khalili@gmail.com>
2021-07-29 17:39:10 +0430

    AC authRequestEdit Request Validator

--- app/Http/Controllers/AC/AuthRequestController.php
+++ app/Http/Controllers/AC/AuthRequestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\AC;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AcAuthRequestEditRequest;
 use App\Models\AuthRequest;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -18,10 +19,17 @@ class AuthRequestController extends Controller
     }
 
     public function edit($id){
+        $authRequest = AuthRequest::query()->find($id)->makeVisible(['try' , 'nc_picture' , 'user_id']);
 
+        return Inertia::render("AC/authRequestEdit" , [
+            'authRequest' => $authRequest
+        ]);
     }
 
-    public function update($id){
+    public function update($id , AcAuthRequestEditRequest $request){
+        $authRequest = AuthRequest::query()->find($id);
+        $authRequest->update($request->all());
 
+        return Inertia::render('AC/authRequestEdit');
     }
 }
