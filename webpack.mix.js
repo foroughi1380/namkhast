a07704751fe87487b1ab863bd9c52d8b8da0257f
Mohammad Foroughi <foroughi1380@gmail.com>
2021-07-29 22:47:38 +0430

    fix ziggy proglem (no define routes)

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,8 @@
+const dir_route = "routes";
 const mix = require('laravel-mix');
 const path = require('path');
+const { exec } = require('child_process');
+const fs = require("fs");
 
 mix.alias({
     ziggy: path.resolve('vendor/tightenco/ziggy/dist/vue'), // or 'vendor/tightenco/ziggy/dist/vue' if you're using the Vue plugin
@@ -15,7 +18,26 @@ mix.alias({
  | file for the application as well as bundling up all the JS files.
  |
  */
-mix.js('resources/js/app.js', 'public/js').vue()
+mix.js('resources/js/app.js', 'public/js')
+    .vue()
+    .before(Mix => { // for normal building
+        exec("php artisan route:cache");
+        exec("php artisan ziggy:generate");
+        fs.readdir("routes" , (err  , files)=>{
+            if (err) console.log("error in listern to routes.");
+            for (let file of files) {
+                console.log("watch to file " + file);
+                fs.watch("routes/" + file , (curr , prev) =>{
+                    console.log(file + " changed.");
+                    exec("php artisan route:cache ; php artisan ziggy:generate");
+                });
+            }
+        });
+    })
+    .after(Mix => { // for watch bilding
+        /*exec("php artisan route:cache");
+        exec("php artisan ziggy:generate");*/
+    })
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
