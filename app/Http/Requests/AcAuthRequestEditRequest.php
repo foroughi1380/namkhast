eb03b1684a7c77390c6ed0e32d30dacc4ef541ee
KryPtoN <sed.mamad.khalili@gmail.com>
2021-07-29 17:40:36 +0430

    authRequestEdit page vue

--- app/Http/Requests/AcAuthRequestEditRequest.php
+++ app/Http/Requests/AcAuthRequestEditRequest.php
@@ -13,7 +13,7 @@ class AcAuthRequestEditRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,7 @@ class AcAuthRequestEditRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            "token" => "captcha:editAuthRequest"
         ];
     }
 }
