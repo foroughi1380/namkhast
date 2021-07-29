e0822b5b273891ba754be5847fc66ef6e3ab4bca
KryPtoN <sed.mamad.khalili@gmail.com>
2021-07-29 16:59:03 +0430

    AC authRequest index method

--- app/Http/Controllers/AC/AuthRequestController.php
+++ app/Http/Controllers/AC/AuthRequestController.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers\AC;
 
 use App\Http\Controllers\Controller;
+use App\Models\AuthRequest;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class AuthRequestController extends Controller
 {
-    //
+    public function index(){
+        $authRequests = AuthRequest::all()->makeVisible(['try' , 'nc_picture' , 'user_id']);
+
+        return Inertia::render("AC/authRequests" , [
+            'authRequests' => $authRequests
+        ]);
+    }
+
+    public function edit($id){
+
+    }
+
+    public function update($id){
+
+    }
 }
