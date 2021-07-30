5790f8fba9f0e81bce311394a538cb6bb3c4dcbb
Mohammad Foroughi <foroughi1380@gmail.com>
2021-07-30 11:17:48 +0430

    added the  Config Control

--- app/Http/Controllers/AC/ConfigController.php
+++ app/Http/Controllers/AC/ConfigController.php
@@ -3,83 +3,75 @@
 namespace App\Http\Controllers\AC;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\configAddRequest;
+use App\Http\Requests\UpdateConfigureRequest;
+use App\Models\Config;
+use \Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Inertia\Inertia;
 
 class ConfigController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Inertia\Response
      */
     public function index()
     {
-        //
+        return Inertia::render("AC/configs" , [
+            "configs" => Config::all()
+            ]
+        );
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Inertia\Response
      */
-    public function create()
+    public function create(configAddRequest $request)
     {
-        //
+        $defaultValue = ['value'=>""];
+        switch ($request->get("type")){
+            case "array": $defaultValue['value'] = []; break;
+            case "boolean": $defaultValue['value'] = false; break;
+            case "float": $defaultValue['value'] = 0; break;
+        }
+        if(Config::create(array_merge($request->only(['name' , 'key' , 'type']) , $defaultValue))){
+            return $this->index();
+        }else{
+            throw ValidationException::withMessages(["unknown"=>"خطای ناشناخته ای رخ داده است"]);
+        }
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Inertia\Response
      */
-    public function store(Request $request)
+    public function store(UpdateConfigureRequest $request)
     {
-        //
-    }
+        $modified = $request->get("modified");
+        $deleted = $request->get("deleted");
+        foreach ($modified as $modify){
+            if (is_array($modify['value'])){
+                $data = [];
+                foreach($modify['value'] as $item){
+                    $data[] = $item['value'];
+                }
+                Config::setWithId($modify['id'] , $data);
+            }else{
+                Config::setWithId($modify['id'] , $modify['value']);
+            }
+        }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+        foreach ($deleted as $del) {
+            Config::deleteWithId($del);
+        }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return $this->index();
     }
 }
