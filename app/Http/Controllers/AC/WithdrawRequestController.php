26c5646c733ff397cd09b51fc8531cb78265b056
KryPtoN <sed.mamad.khalili@gmail.com>
2021-07-30 13:32:18 +0430

    AC withdrawRequest controller index method

--- app/Http/Controllers/AC/WithdrawRequestController.php
+++ app/Http/Controllers/AC/WithdrawRequestController.php
@@ -1,10 +1,25 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\AC;
 
+use App\Models\WithdrawRequest;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Inertia\Inertia;
 
 class WithdrawRequestController extends Controller
 {
-    //
+    public function index()
+    {
+        $wdRequests = WithdrawRequest::all()->makeVisible(['user_id']);
+
+        return Inertia::render("AC/withdrawRequest" , [
+            'wdRequests' => $wdRequests
+        ]);
+    }
+
+    public function changeStatus($id)
+    {
+
+    }
 }
