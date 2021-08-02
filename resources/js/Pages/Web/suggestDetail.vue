4d84253178d8c4473db5fbb9ba8de907d055fc8a
KryPtoN <sed.mamad.khalili@gmail.com>
2021-08-02 17:31:23 +0430

    suggest detail vue page

--- resources/js/Pages/Web/suggestDetail.vue
+++ resources/js/Pages/Web/suggestDetail.vue
@@ -11,36 +11,46 @@
           <div class="row">
             <div class="d-flex justify-content-center align-items-center mb-1 mx-auto">
               <div class="avatar mr-50">
-                <img src="/theme/web/app-assets/images/portrait/small/avatar-s-5.jpg" alt="avtar img holder" height="75" width="75">
+                <img :src="user.picture" alt="avtar img holder" height="75" width="75">
               </div>
               <div class="user-page-info">
-                <h3 class="mb-0">نام کاربری</h3>
+                <h3 class="mb-0">{{ user.name}} {{ user.family }}</h3>
               </div>
             </div>
           </div>
           <div class="text-left my-2">
             <div class="col-12 mx-auto text-center my-4">
               <h5>نام پیشنهادی :</h5>
-              <h1> نام</h1>
+              <h1> {{ suggest.suggested_name }}</h1>
             </div>
           </div>
 
-          <div class=" my-2">
+          <div class="text-left my-2" v-if="suggest.description">
+            <div class="col-12 mx-auto text-center my-4">
+              <h5>توضیحات :</h5>
+              <p> {{ suggest.description }}</p>
+            </div>
+          </div>
+
+          <div class=" my-2" v-if="suggest.sound">
             <div class="col-12 mx-auto text-center my-4">
               <h5>فایل صوتی تلفظ :</h5>
               <div class="col-md-12">
                 <div class="card">
                   <!-- AUDIO -->
                   <audio id="plyr-audio-player" class="audio-player ml-4" controls>
-                    <source src="https://cdn.plyr.io/static/demo/Kishi_Bashi_-_It_All_Began_With_a_Burst.mp3" type="audio/mp3" />
+                    <source :src="suggest.sound" type="audio/mp3" />
                   </audio>
                   <!-- AUDIO END -->
+                  <p class="text-danger " v-for="error in errors" v-text="error"></p>
+
                 </div>
               </div>
             </div>
           </div>
           <div class="text-center mx-auto">
-            <a class="btn btn-primary text-white">اعلام کاربر به عنوان برنده چالش</a>
+            <WaitButton text="اعلام کاربر به عنوان برنده چالش" :wait="inProcess" :on-click="choiceWinner" :disabled="inProcess"/>
+
           </div>
         </div>
       </div>
@@ -51,10 +61,46 @@
 
 <script>
 import appLayout from "../../Shared/appLayout";
+import {Inertia} from "@inertiajs/inertia";
+import WaitButton from "../../Shared/Components/WaitButton";
 
 export default {
   name: "suggestDetail",
-  layout: appLayout
+  layout: appLayout,
+  components : { WaitButton },
+  data() {
+    return {
+      winnerUser: "",
+      token: "",
+      inProcess: false
+    }
+  },
+  props: {
+    user: {},
+    suggest: {},
+    errors: {}
+  },
+  methods:{
+    choiceWinner(){
+      console.log('hi')
+        this.inProcess = true;
+
+        this.generateRecaptchaToken('choiceWinner').then(token => {
+
+          Inertia.post(this.route('challenge.winner' , this.$props.suggest.id), {
+            winnerUser: this.$props.suggest.user_id,
+            token:token,
+          }, {
+            onSuccess: page => {
+              this.toast("کاربر به عنوان برنده اعلام شد" , "success")
+            },
+            onFinish: visit => {
+              this.inProcess = false;
+            }
+          })
+        });
+      }
+  }
 }
 </script>
 
