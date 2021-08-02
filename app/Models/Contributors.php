a805755fddc38cd0cede74a382548737f2299584
KryPtoN <sed.mamad.khalili@gmail.com>
2021-08-02 17:30:42 +0430

    Contributors Model

--- app/Models/Contributors.php
+++ app/Models/Contributors.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Contributors extends Model
 {
     use HasFactory;
+
+    protected $table = 'contributors';
+
+    protected $fillable = ["challenge_id" , "user_id" , "transaction_id" ,"suggested_name" , "sound" , "description"];
 }
