84d1aed63c52aa2fd4add3aeac00c0f04d3ed997
Mohammad Foroughi <foroughi1380@gmail.com>
2021-08-10 05:43:48 +0430

    Fix #15

--- app/Http/Controllers/web/WithdrawRequestController.php
+++ app/Http/Controllers/web/WithdrawRequestController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\web;
 
 use App\Http\Controllers\Controller;
+use App\Models\Wallet;
 use App\Models\WithdrawRequest;
 use App\Http\Requests\WithdrawRequest as wdRequest;
 use App\Models\AuthRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
 use Inertia\Inertia;
 
 class WithdrawRequestController extends Controller
@@ -22,9 +24,22 @@ class WithdrawRequestController extends Controller
     }
 
     public function submitRequest(wdRequest $request){
-        //TODO: Check User Have Enough Balance To Withdraw Later
+        if (Wallet::query()->where("user_id" , Auth::id())->sum("price") < $request->get("price")){
+            throw ValidationException::withMessages([
+               "price" => "اعتبار حساب شما کافی نمیباشد."
+            ]);
+        }
+        $wr = WithdrawRequest::create(['user_id'=>$request->user()->id,'price'=>$request->get('price')]);
+        if($wr) {
+            $wallet = new Wallet();
+            $wallet->user_id = Auth::id();
+            $wallet->price = $request->get("price") * -1;
+            $wallet->extras = [
+                "for" => WithdrawRequest::class,
+                "id" => $wr->id
+            ];
+            $wallet->save();
 
-        if(DB::table('withdraw_request')->insert(['user_id'=>$request->user()->id,'price'=>$request->get('price')])) {
             return redirect('/withdraw/track');
         }else{
             return Inertia::render("Web/withdraw" , [
