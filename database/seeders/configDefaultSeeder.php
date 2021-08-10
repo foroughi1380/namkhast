6b53d15e1f211413a5c69013be8e5fb6df7d7a96
Mohammad Foroughi <foroughi1380@gmail.com>
2021-08-10 08:08:15 +0430

    Fix bog in #19

--- database/seeders/configDefaultSeeder.php
+++ database/seeders/configDefaultSeeder.php
@@ -14,7 +14,7 @@ class configDefaultSeeder extends Seeder
      */
     public function run(){
         Config::create(['name'=>'برسی کپچا های ورودی'           , 'key' => 'check_captcha'      ,   'type'=>'boolean'   , 'value' => "1"]);
-        Config::create(['name'=>'دسته بندی های چالش ها'         , 'key' => 'challenge_group'    ,   'type'=>'array'     , 'value' => ["شرکت","پروژه","نرم افزار","اختراع","جا و مکان" , "تیم - گروه" , "خدمات" , "کتاب - مقالات" , "وبسایت" , "فیلم و -یدئو" , "اهنگ - صوت - پادکست" , "انسان - بچه" , "حیوانات" , "اشیاء" , "دیگر دسته بندی ها"]]);
+        Config::create(['name'=>'دسته بندی های چالش ها'         , 'key' => 'challenge_group'    ,   'type'=>'array'     , 'value' => ["شرکت","پروژه","نرم افزار","اختراع","جا و مکان" , "تیم ~ گروه" , "خدمات" , "کتاب ~ مقالات" , "وبسایت" , "فیلم و ~یدئو" , "اهنگ ~ صوت ~ پادکست" , "انسان ~ بچه" , "حیوانات" , "اشیاء" , "دیگر دسته بندی ها"]]);
         Config::create(['name'=>'نرخ های شرکت در چالش پولی'     , 'key' => 'challenge_pp'       ,   'type'=>'array'     , 'value' => ["1000","2000","3000","4000","5000"]]);
         Config::create(['name'=>'حداقل مبلغ جایزه در چالش ها'   , 'key' => 'min_coast_budget'   ,   'type'=>'float'     , 'value' => 5000]);
         Config::create(['name'=>'حداکثر مبلغ جایزه در چالش ها'  , 'key' => 'max_coast_budget'   ,   'type'=>'float'     , 'value' => 40000000]);
