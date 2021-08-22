dc17a1a31959f2b202969905415d55b683fae8c4
Mohammad Foroughi <foroughi1380@gmail.com>
2021-08-22 09:20:01 +0430

    Configuration PWA

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -39,6 +39,7 @@
             background-size: cover;
         }
     </style>
+    @laravelPWA
 </head>
 <body class="menu-collapsed vertical-layout vertical-menu-modern semi-dark-layout 2-columns  navbar-floating footer-static  " data-open="click" data-menu="vertical-menu-modern" data-col="2-columns" data-layout="semi-dark-layout">
 @inertia
---
+++ resources/views/vendor/laravelpwa/offline.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>خطا در دستری به نام خواست لطفا اینترنت خود را متصل بفرمایید.</h1>
+
+@endsection
